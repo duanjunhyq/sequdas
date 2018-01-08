4931aa4ba71e53293ecac2535e276e89e1123086
duanjunhyq <duanjun1981@gmail.com>
2018-01-08 11:22:45 -0800

    Update sample_sheet.py

--- sequdas_client/Lib/sample_sheet.py
+++ sequdas_client/Lib/sample_sheet.py
@@ -6,6 +6,7 @@ from collections import OrderedDict
 import json
 from copy import deepcopy
 from fnmatch import translate as fn_translate
+import sys
 
 def get_csv_reader(sample_sheet_file):
 
@@ -98,7 +99,10 @@ def parse_metadata(sample_sheet_file):
         elif section is "header":
             try:
                 key_name = metadata_key_translation_dict[line[0]]
-                metadata_dict[key_name] = line[1]
+                if(len(line)>1):
+                    metadata_dict[key_name] = line[1]
+                else:
+                    metadata_dict[key_name] = ""
             except KeyError:
                 logging.info("Unexpected key in header: [{}]".format(line[0]))
         elif section is "reads":
@@ -285,34 +289,41 @@ def parse_samples(sample_sheet_file):
     # fill in values for keys. line is currently below the [Data] headers
     for sample_number, line in enumerate(csv_reader):
 
-        if len(sample_dict.keys()) != len(line):
-            """
-            if there is one more Data header compared to the length of
-            data values then add an empty string to the end of data values
-            i.e the Description will be empty string
-            assumes the last Data header is going to be the Description
-            this handles the case where the last trailing comma is trimmed
-
-            Shaun said this issue may come up when a user edits the
-            SampleSheet from within the MiSeq software
-            """
-            if len(sample_dict.keys()) - len(line) == 1:
-                line.append("")
-            else:
-                raise SampleSheetError(
-                    "Number of values doesn't match number of " +
-                    "[Data] headers. " +
-                    ("Number of [Data] headers: {data_len}. " +
-                     "Number of values: {val_len}").format(
-                        data_len=len(sample_dict.keys()),
-                        val_len=len(line)
-                    ), [("Your sample sheet is malformed. I expected to find {} "
-                         "columns the [Data] section, but I only found {} columns "
-                         "for line {}.".format(len(sample_dict.keys()), len(line), line))]
-                )
+#        if len(sample_dict.keys()) != len(line):
+#            """
+#            if there is one more Data header compared to the length of
+#            data values then add an empty string to the end of data values
+#            i.e the Description will be empty string
+#            assumes the last Data header is going to be the Description
+#            this handles the case where the last trailing comma is trimmed
+#
+#            Shaun said this issue may come up when a user edits the
+#            SampleSheet from within the MiSeq software
+#            """
+#            if len(sample_dict.keys()) - len(line) == 1:
+#                line.append("")
+#            else:
+#                print sample_dict.keys()
+#                print line
+#                print len(sample_dict.keys())
+#                print len(line)
+#                sys.exit()
+#                print "error: Number of values doesn't match number of"+"[Data] headers. " +("Number of [Data] headers: {data_len}. " +"Number of values: {val_len}").format(data_len=len(sample_dict.keys()), val_len=len(line)
+#                #print "Your sample sheet is malformed. I expected to find {} "
+#                         "columns the [Data] section, but I only found {} columns "
+#                         "for line {}.".format(len(sample_dict.keys()), len(line), line))]
+              
 
         for index, key in enumerate(sample_dict.keys()):
-            sample_dict[key] = line[index].strip()  # assumes values are never empty
+            if len(key)>0:
+                if index>=len(line)-1:
+                    sample_dict[key] = ""
+                else:
+                    sample_dict[key] = line[index].strip()  # assumes values are never empty
+#                print index
+#                print len(line)
+#                print sample_dict[key]
+#                print line[index].strip()
 
         if len(sample_dict["sampleName"]) == 0:
             sample_dict["sampleName"] = sample_dict["sequencerSampleId"]
@@ -410,9 +421,9 @@ def correct_samplename_with_underscore(fastq_file_list, sample_name):
     pair_file_list = []
     if re.search( r'\_', sample_name):
         sample_name_n=sample_name.replace("_", "-")
-    pattern1 = re.escape(sample_name) + "_S\\d+_L\\d{3}_R(\\d+)_\\S+\\.fastq.*$"
-    for fastq_file in fastq_file_list:
-        match = re.search(pattern1, fastq_file)
-        if match is not None:
-            ret = re.sub(sample_name,sample_name_n, fastq_file)
-            os.rename(fastq_file, ret)
+        pattern1 = re.escape(sample_name) + "_S\\d+_L\\d{3}_R(\\d+)_\\S+\\.fastq.*$"
+        for fastq_file in fastq_file_list:
+            match = re.search(pattern1, fastq_file)
+            if match is not None:
+                ret = re.sub(sample_name,sample_name_n, fastq_file)
+                os.rename(fastq_file, ret)
