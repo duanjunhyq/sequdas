544528b72ab2b2a3be5c83262912d3f970018eca
duanjunhyq <duanjun1981@gmail.com>
2018-01-08 11:21:18 -0800

    Update core.py

--- sequdas_client/Lib/core.py
+++ sequdas_client/Lib/core.py
@@ -22,6 +22,8 @@ def sequdas_config():
     except Exception as e :
         print(str(e),' Could not read configuration file')
 
+
+
 def read_config():
     config = ConfigParser.RawConfigParser()
     pathname = os.path.dirname(os.path.abspath(sys.argv[0]))
@@ -32,7 +34,7 @@ def read_config():
     except Exception as e :
         print(str(e))
 
-def validate_email(email): 
+def validate_email_address(email): 
     from validate_email import validate_email
     is_valid = validate_email(email)
     #match=re.search(r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9]+\.[a-zA-Z0-9.]*\.*[ca|com|org|edu]{3}$)",email)
@@ -72,6 +74,13 @@ def SubDirPath(dirname):
         if os.path.isdir(check_dir):
             folder.append(check_dir)
     return folder
+
+def check_create_folder(dirname):
+    if not os.path.exists(dirname):
+        try:
+            os.makedirs(dirname)
+        except:
+            print "make directory error, please check "+dirname
 
 def is_number(s):
     try:
@@ -178,18 +187,31 @@ def doUpdate(bccdc_id_value,status_value):
     cur.execute(("UPDATE information SET status=%s,end_time=%s WHERE bccdc_id=%s"),(status_value,timestamp,bccdc_id_value))
     myConnection.commit()
     myConnection.close()
-    
-def md5_compare(data_server,run_handle,next_ID,data_dir_server,logfile_dir):
-    f_remote = open(logfile_dir+"remote.md5.tmp", "w")
-    f_compare = open(logfile_dir+"compare_result.tmp", "w")
-    location_remotemd5=logfile_dir+"remote.md5.tmp"
-    handle_ID=next_ID
-    final_md5=logfile_dir+handle_ID+"_md5"
-    error_md5=logfile_dir+handle_ID+"_error_md5"
-    location_compare_result=logfile_dir+"compare_result.tmp"
-    if os.path.isdir(run_handle):
-        remotemd5 = subprocess.call(["ssh", data_server, "md5deep","-r","-s",data_dir_server],stdout=f_remote);
-        local_md5_compare=subprocess.call(["md5deep","-r","-X",location_remotemd5,run_handle],stdout=f_compare);
+
+def md5_compare(data_server,run_path,run_id,data_dir_server,logfile_dir):
+    f_remote = open(logfile_dir+run_id+"remote.md5.tmp", "w")
+    f_compare = open(logfile_dir+run_id+"compare_result.tmp", "w")
+    location_remotemd5=logfile_dir+run_id+"remote.md5.tmp"
+    final_md5=logfile_dir+run_id+"_md5"
+    error_md5=logfile_dir+run_id+"_error_md5"
+    location_compare_result=logfile_dir+run_id+"compare_result.tmp"
+    if os.path.isdir(run_path):
+        remotemd5 = subprocess.Popen(["ssh", data_server, "md5deep","-r","-s",data_dir_server],stdout=f_remote);
+        remotemd5.wait()
+        matchObj = re.match( r'\/\/', run_path)
+        ###handle issue with net mapped drive "//" cannot be recognized by md5deep in cygwin
+        if matchObj:
+            folder_name= os.path.basename(os.path.normpath(run_path))
+            cmd_create="ln -s "+run_path+" "+logfile_dir
+            os.system(cmd_create)
+            local_md5_compare=subprocess.Popen(["md5deep","-r","-X",location_remotemd5,logfile_dir+folder_name],stdout=f_compare)
+            local_md5_compare.wait()
+            cmd_delete="rm -fr "+logfile_dir+folder_name
+            os.system(cmd_delete) 
+        else:
+            local_md5_compare=subprocess.Popen(["md5deep","-r","-X",location_remotemd5,run_path],stdout=f_compare);
+            local_md5_compare.wait()
+            
     else:
         print "please check your remote path when comparing md5"
     if os.path.exists(location_compare_result) and os.path.getsize(location_compare_result) > 0:
@@ -215,7 +237,27 @@ def check_path_with_slash(folder):
 def del_end_slash(folder):
     if folder.endswith("/"):
         folder=re.sub('/$', '', folder)
-    return folder   
+    return folder
+    
+def get_archiving_time_db(sequdas_id):
+    s_config=sequdas_config()
+    ID_prefix=s_config['basic']['run_id_prefix']
+    mysql_host=s_config['mysql_account']['mysql_host']
+    mysql_user=s_config['mysql_account']['mysql_user']
+    mysql_passwd=s_config['mysql_account']['mysql_passwd']
+    mysql_db=s_config['mysql_account']['mysql_db']
+    myConnection = MySQLdb.connect( host=mysql_host, user=mysql_user, passwd=mysql_passwd, db=mysql_db)
+    cur = myConnection.cursor()
+    sql = "SELECT start_time FROM `status_table` WHERE bccdc_id = '%s'" % (sequdas_id)
+    cur.execute(sql)
+    data = cur.fetchone()
+    cur.close()
+    #print data[0]
+    if(data):
+        return data[0]
+    else:
+        return "error"
+ 
 def del_old_file(filename):
     s_config=sequdas_config()
     logfile_dir=s_config['basic']['logfile_dir']    
@@ -239,23 +281,26 @@ def del_old_file(filename):
         m = re.match("8", line.split("\t")[5])
         if m:
             file_archiving_time=line.split("\t")[4]
-            #print file_archiving_time
-            pattern = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}#[0-9:]{8}')
-            match=pattern.match(file_archiving_time)
-            if match:                
-                file_archiving_time_N=match.group()
-                if(judge_file_time(file_archiving_time_N)>old_file_days_limit):
-                    my_list.append(line.split("\t")[0])
-                    timestamp = time.strftime("%Y-%m-%d#%H:%M:%S")	
-                    ################################################
-                    if os.path.isdir(line.split("\t")[2]):
-                        shutil.rmtree(line.split("\t")[2])
-                    elif os.path.isfile(line.split("\t")[2]):
-                        os.remove(line.split("\t")[2])
-                    else:
-                        print "please check your file path"+line.split("\t")[2]
+            file_archiving_time_db=get_archiving_time_db(line.split("\t")[0])
+            if file_archiving_time == file_archiving_time_db and len(file_archiving_time_db) >5:
+                pattern = re.compile('[0-9]{4}-[0-9]{2}-[0-9]{2}#[0-9:]{8}')
+                match=pattern.match(file_archiving_time)
+                if match:                
+                    file_archiving_time_N=match.group()
+                    if(judge_file_time(file_archiving_time_N)>old_file_days_limit):
+                        my_list.append(line.split("\t")[0])
+                        timestamp = time.strftime("%Y-%m-%d#%H:%M:%S")	
+                        ################################################
+                        if os.path.isdir(line.split("\t")[2]):
+                            shutil.rmtree(line.split("\t")[2])
+                        elif os.path.isfile(line.split("\t")[2]):
+                            os.remove(line.split("\t")[2])
+                        else:
+                            print "please check your file path"+line.split("\t")[2]
+                else:
+                    print "Please check the time format"
             else:
-                print "Please check the time format"
+                print line.split("\t")[0]+": The time in log file is different from the time in db"
     i.close()
     logfile_dir_without_slash=del_end_slash(logfile_dir)
     if(len(my_list)>0):
@@ -263,7 +308,8 @@ def del_old_file(filename):
         status_id_str=str(status_id)            
         for item in my_list:
             change_logfile(logfile,item,status_id_str)
-            subprocess.call(["rsync","-p","--chmod=ug=rwx","-artvh",logfile_dir_without_slash,data_server+":"+server_log_dir],stdout=subprocess.PIPE,stderr=subprocess.PIPE)
+            subprocess.call(["rsync","-p","--chmod=ug=rwx","-artvh",logfile_dir_without_slash,data_server+":"+server_log_dir])
+    return my_list
 
 def remove_csv_file():
     files = os.listdir(os.curdir)
